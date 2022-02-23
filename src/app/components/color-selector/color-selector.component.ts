b89219be9937f351866825167b16ce49fdce7d72
JMF <jm.ficart@gmail.com>
2022-02-23 16:58:44 +0100

    23/02/2022 16:58

--- src/app/components/color-selector/color-selector.component.ts
+++ src/app/components/color-selector/color-selector.component.ts
@@ -7,36 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorSelectorComponent implements OnInit {
   couleurpion = ["pink", "green", "blue", "red", "purple", "yellow", "brown"];
+  combinaison = ["green", "blue", "blue", "red"];
   pcolor = ["white", "white", "white", "white"];
-  ptrcouleur: number = 0;
+  eColor: Array<string[]> = [];
+  eBGColor: Array<string[]> = [];
+  nbreEssaisPossibles: number = 3; //10;
+  cptCouleur: number = 0;
+  cptEssai:number = 0;
   
-  oTable: HTMLTableElement;
-  oTable = document.getElementById('tblresultat');
-  // aoBts = this.oTable.getElementsByClassName('bt');
-
-  constructor() { }
+  constructor() {
+    for(var i=0;i<3;i++){
+      this.eColor.push(["","","",""]);
+      this.eBGColor.push(["white", "white", "white", "white"]);
+    }
+  }
 
   ngOnInit(): void {
   }
 
   validate() {
-    console.log("Validate")
-    // Copier les couleurs dans la table resultat.
-    // this.aoBts = this.oTable.getElementsByClassName('bt');
-    // for(var i = 0; i<this.aoBts.length; i++){
-    //   aoBts[i].addEventListener('click',  deplaceColonne);
-    // }
+    if (this.cptCouleur == 4){
+      // Copier les couleurs dans la table resultat.
+      for(var i = 0; i<4; i++){
+        this.eColor[this.cptEssai][i]=this.pcolor[i];
+      }
+
+      if(this.checkResult() == true){
+        alert('Vous avez gagné !');
+      } else
+      {
+        this.cptEssai++;
+        if (this.cptEssai==3) {
+          alert('Vous avez perdu !');
+        }
+      } 
+      this.reset();
+    }
   }
 
   reset() {
-    this.ptrcouleur = 0;
+    this.cptCouleur = 0;
     this.pcolor = ["white", "white", "white", "white"];
   }
 
   choosecolor(i: number) {
-    if (this.ptrcouleur <= 3) {
-      this.pcolor[this.ptrcouleur] = this.couleurpion[i];
-      this.ptrcouleur++;
+    if (this.cptCouleur <= 3) {
+      this.pcolor[this.cptCouleur] = this.couleurpion[i];
+      this.cptCouleur++;
     }
   }
+
+  checkResult():boolean {
+    return false;
+  }
+
 }
